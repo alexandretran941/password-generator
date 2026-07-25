c3f940d5480e672f871e0092431a29bddb1dd5a9
Alexandre TRAN <alexandre.tran941@gmail.com>
2026-07-25 15:55:34 +0200

    Ajout des options

--- script.js
+++ script.js
@@ -2,23 +2,53 @@ function generatePassword(){
 
     let characters =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
-
-
-    let password = "";
-
-    let length = 12;
-
-
-    for(let i = 0; i < length; i++){
-
-        let random =
-        Math.floor(Math.random() * characters.length);
-
-        password += characters[random];
-
+    
+    if (document.getElementById("uppercase").checked) {
+        characters += "ABCDEFGHIJKMNOPQRSTUVWXYZ";
     }
 
+    if (document.getElementById("numbers").checked) {
+        characters += "01234567890";
+    }
 
+    if (document.getElementById("symbols").checked) {
+        characters += "!@#$%^&*";
+    }
+    
+    const length = Number(document.getElementById("length").value);
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const random = Math.floor(Math.random() * characters.length);
+
+        password += charcters[random];
+    }
+    
     document.getElementById("password").value = password;
 
+    checkStrength(password);
+}
+
+function copyPassword() {
+
+    const password = document.getElementById("password").value;
+
+    navigator.clipboard.writeText(password);
+
+    alert("Mot de passe copié !");
+}
+
+function checkStrength(password) {
+    const strength = document.getElementById("strength");
+    
+if (password.length < 8) {
+
+    strength.textContent = "Faible";
+
+} else if (password.length < 12) {
+
+    strength.textContent = "Moyen";
+} else {
+    strength.textContent = "Fort";
+}
 }
